61c3efa8b60f6d16a9db282ad5dce5b9320703e1
LAPTOP-0VJIAJQ5\99450 <abdurahmanov.togrul@gmail.com>
2020-08-13 17:46:02 +0400

    yalnish istek atma yontem redux-thunk kullanmadan

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,11 @@
-export const fetchPostList=()=>{
+import axios from 'axios';
+
+export const fetchPostList=async()=>{
+
+    const response=await axios.get('https://jsonplaceholder.typicode.com/posts');
+
     return {
         type:'FETCH_POST_LIST',
-        payload:[]
-    }
+        payload:response
+    };
 }
--- blog/src/component/postList.js
+++ blog/src/component/postList.js
@@ -2,6 +2,12 @@ import React ,{ Component} from 'react';
 import { connect } from 'react-redux';
 import { fetchPostList } from '../actions';
 class PostList extends Component{
+
+    componentDidMount(){
+        this.props.fetchPostList();
+    }
+
+
     render(){
         return(
             <div>
