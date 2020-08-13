dc397fcaf99052fad200202a8912c9d9edb94d3f
LAPTOP-0VJIAJQ5\99450 <abdurahmanov.togrul@gmail.com>
2020-08-13 17:48:47 +0400

    middleware configure to project

--- blog/src/index.js
+++ blog/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/app';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from './reducers'
+import {createStore , applyMiddleware} from 'redux';
+import thunk from 'react-redux';
+import reducers from './reducers';
 
 
 
+const store = createStore(reducers,applyMiddleware(thunk));
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.querySelector('#root')
